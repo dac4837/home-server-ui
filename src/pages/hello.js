7421b52aedf4908ab10a45a3a88be56ebf90934b
David Collins <dacollins3@gmail.com>
2023-03-29 21:49:01 -0400

    Updates message text

--- src/pages/hello.js
+++ src/pages/hello.js
@@ -1,19 +1,17 @@
 export default function Hello() {
   return (
     <div className="container">
-      <h1 className="text-center">Send a message to James!</h1>
+      <h1 className="text-center">James Guestbook</h1>
       <div class="alert alert-light" role="alert">
-        Use this site to write a message for James when he's older. Messages are
-        not guaranteed to be read at desired age and may be read by David/Pearly
-        in advance
+        Share a memory or message for James to cherish when he becomes an adult!
       </div>
 
-      <form>
-        <div className="mb-3 w-25 ">
+      <form method="post">
+        <div className="mb-3 w-75 ">
           <label for="name" class="form-label">
             Your name
           </label>
-          <input type="text" className="form-control" id="name" />
+          <input type="text" className="form-control" id="name" name="name" required />
         </div>
         <div className="mb-3 w-75">
           <label for="message" class="form-label">
@@ -22,6 +20,9 @@ export default function Hello() {
           <textarea
             className="form-control"
             id="message"
+            name="message"
+            placeholder="Share a story, send a wish, or tell 'future James' about his current childhood self! (Favorite activities, what makes him laugh, etc)."
+            required
             style={{ height: "300px" }}
           />
         </div>
