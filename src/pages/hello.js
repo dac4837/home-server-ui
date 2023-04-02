3d27902b732b6ee3773b8672a52a78bf9384a6b8
David Collins <dacollins3@gmail.com>
2023-04-02 17:29:35 -0400

    using state and axios

--- src/pages/hello.js
+++ src/pages/hello.js
@@ -1,32 +1,83 @@
+import { useState } from "react";
+import axios from "axios";
+
 export default function Hello() {
+
+  const [name, setName ] = useState("")
+  const [message, setMessage ] = useState("")
+  const [successful, setSuccessful] = useState()
+  const [errorMessage, setErrorMessage] = useState()
+  
+  const successAlert=( 
+    successful && 
+    <div className="alert alert-success" role="alert">
+      Success!
+       </div> 
+  )
+
+  const errorAlert= ( 
+    errorMessage && 
+    <div className="alert alert-danger" role="alert">
+      Error: {errorMessage}
+       </div> 
+  )
+
+  const clearMessages = () => {
+    setSuccessful(false)
+    setErrorMessage(null)
+  }
+
+  const onInputChange = (input, event) => {
+    clearMessages()
+    input(event.target.value)
+
+  }
+
+  const onSubmit = () => {
+    axios.post('/hijames', {name, message})
+      .then(function (response) {
+        clearMessages()
+        setSuccessful(true)
+        setName("")
+        setMessage("")
+      })
+      .catch(function (response) { 
+        clearMessages()
+        const errorMessage = response?.response?.data ? response.response.data : "Error submitting request"
+
+        setErrorMessage(errorMessage)
+      });
+  }
+
   return (
     <div className="container">
-      <h1 className="text-center">James Guestbook</h1>
-      {/* <div class="alert alert-light" role="alert">
-        Share a memory or message for James to cherish when he becomes an adult!
-      </div> */}
+      <h1 className="text-center">Messages for James!</h1>
+      {successAlert}
+      {errorAlert}
 
-      <form method="post">
+      <form>
         <div className="mb-3 w-75 ">
-          <label for="name" class="form-label">
+          <label for="name" className="form-label">
             Your name
           </label>
-          <input type="text" className="form-control" id="name" name="name" required />
+          <input type="text" className="form-control" id="name"
+          value={name} onChange={(change)=> onInputChange(setName, change)} required />
         </div>
         <div className="mb-3 w-75">
-          <label for="message" class="form-label">
+          <label for="message" className="form-label">
             Message
           </label>
           <textarea
             className="form-control"
             id="message"
-            name="message"
+            value={message}
+            onChange={(change) => onInputChange(setMessage, change)}
             placeholder="Share a story, send a wish, or tell 'future James' about his current childhood self! (Favorite activities, what makes him laugh, etc)."
             required
             style={{ height: "300px" }}
           />
         </div>
-        <button type="submit" className="btn btn-primary">
+        <button type="button" className="btn btn-primary" onClick={onSubmit}>
           Submit
         </button>
       </form>
